685541d0da8a03fdabd7711426ceb2587a5881a7
Stanojkovic <sasa.stanojkovic@masfak.ni.ac.rs>
2018-04-30 11:28:54 +0200

    Change paragraph on About pages !

--- WebShop/Models/IdentityModels.cs
+++ WebShop/Models/IdentityModels.cs
@@ -9,6 +9,8 @@ namespace WebShop.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string Ime { get; set; }
+        public int Prezime { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -20,6 +22,8 @@ namespace WebShop.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public virtual DbSet<Proizvod> Proizvodi { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
---
+++ WebShop/Models/Proizvod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShop.Models
+{
+    public class Proizvod
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public double Cena { get; set; }
+        public string Kategorija { get; set; }
+    }
+}
